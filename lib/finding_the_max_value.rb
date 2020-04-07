62654ba64754f7c7ab8ab03eba6a8662e8109706
anna bateman <annacbateman@gmail.com>
2020-04-07 22:26:32 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -8,15 +8,15 @@ def find_max_value(array)
   # sorted_array = array.sort
   # puts sorted_array.pop
   
-# count = 0
-#   found_value_index = nil
-#   while count < array.length do
-#     if array[count] == value_to_find
-#       found_value_index = count
-#     end
-#     count += 1
-#   end
-#   found_value_index
+count = 0
+  found_value_index = nil
+  while count < array.length do
+    if array[count] == value_to_find
+      found_value_index = count
+    end
+    count += 1
+  end
+  found_value_index
   
   
 end
