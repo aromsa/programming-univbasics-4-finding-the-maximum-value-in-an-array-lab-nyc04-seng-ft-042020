2d7a178e07ca726b65606043092602046b619245
anna bateman <annacbateman@gmail.com>
2020-04-07 22:13:16 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,11 +1,19 @@
 def find_max_value(array)
   # Add your solution here
+  sorted_find_max_value = find_max_value.sort
   
   
   
   
-  
-?
+# count = 0
+#   found_value_index = nil
+#   while count < array.length do
+#     if array[count] == value_to_find
+#       found_value_index = count
+#     end
+#     count += 1
+#   end
+#   found_value_index
   
   
 end
