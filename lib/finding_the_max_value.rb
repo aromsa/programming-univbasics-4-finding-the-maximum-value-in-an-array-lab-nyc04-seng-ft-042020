23a867bb69ad3bc7161ef014ff3266cd13ec49bf
anna bateman <annacbateman@gmail.com>
2020-04-07 22:12:28 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,15 +2,10 @@ def find_max_value(array)
   # Add your solution here
   
   
-  count = 0
-  found_value_index = nil
-  while count < array.length do
-    if array[count] == value_to_find
-      found_value_index = count
-    end
-    count += 1
-  end
-  found_value_index
+  
+  
+  
+?
   
   
 end
